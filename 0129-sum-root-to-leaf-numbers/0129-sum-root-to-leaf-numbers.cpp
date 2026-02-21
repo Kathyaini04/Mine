e28eca5708e1117e786911f1400b9ec01269b4b3
Kathyaini04 <kathyainiyelmelwar@gmail.com>
2026-02-21 16:42:35 +0530

    Time: 0 ms (100%), Space: 13.4 MB (5.73%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,13 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sumNumbers(TreeNode* root) {
-        return dfs(root, 0);
+    void solve(TreeNode* root, vector<string>& st, string s) {
+        if(!root) return;
+        s += to_string(root->val);
+        if(!root->right && !root->left) {
+            st.push_back(s);
+            return;
+        }
+        if(root->left) solve(root->left, st, s);
+        if(root->right) solve(root->right, st, s);
     }
-
-    int dfs(TreeNode* node, int path) {
-        if (!node) return 0;
-        path = path * 10 + node->val;
-        if (!node->left && !node->right) return path;
-        return dfs(node->left, path) + dfs(node->right, path);
+    int sumNumbers(TreeNode* root) {
+        vector<string> st;
+        int ans = 0;
+        solve(root, st, "");
+        for(string s : st) {
+            ans += stoi(s);
+        }
+        return ans;
     }
 };
